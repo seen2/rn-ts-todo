6b65ab1f33a149ca0bef526bbd73c7368e3adbae
seen2 <tiwarietc@gmail.com>
2022-08-09 06:49:19 +0530

    TodosScreen View fixed

--- src/screens/todos/TodosScreen.tsx
+++ src/screens/todos/TodosScreen.tsx
@@ -1,47 +1,40 @@
-import { View, Text, Switch, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useState, useEffect } from 'react';
-import { useTheme } from '@react-navigation/native';
+import { View, Text, Switch, FlatList } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { useState, useEffect } from 'react'
+import { useTheme } from '@react-navigation/native'
 
-import CCard from '../../components/CCard';
-import CButton from '../../components/CButton';
-import Logo from '../../components/Logo';
-import type { ITodo } from '../../types/appTypes';
-import { onGetTodos } from '../../redux/actionCreators/todoActions';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import CCard from '../../components/CCard'
+import CButton from '../../components/CButton'
+import Logo from '../../components/Logo'
+import type { ITodo } from '../../types/appTypes'
+import { onGetTodos } from '../../redux/actionCreators/todoActions'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
 
 export default function TodosScreen() {
-  const dispatch = useAppDispatch();
-  const changedData: ITodo[] = useAppSelector((state) => state.todos);
-  const [data, SetData]: [data: ITodo[] | [], SetData: Function] = useState(
-    useAppSelector((state) => state.todos)
-  );
+  const dispatch = useAppDispatch()
+  const changedData: ITodo[] = useAppSelector((state) => state.todos)
+  const [data, SetData]: [data: ITodo[] | [], SetData: Function] = useState(useAppSelector((state) => state.todos))
 
   useEffect(() => {
-    dispatch(onGetTodos());
+    dispatch(onGetTodos())
 
     return () => {
-      SetData(changedData);
-      
-    };
-  }, [dispatch, SetData, changedData]);
+      SetData(changedData)
+    }
+  }, [dispatch, SetData, changedData])
 
   return (
-    <View>
+    <View style={{marginBottom:50}}>
       <Logo />
-      <FlatList
-        data={data}
-        renderItem={({ item }: { item: ITodo }) => (
-          <TodoCard key={item._id} item={item} />
-        )}
-      />
+      <FlatList data={data} renderItem={({ item }: { item: ITodo }) => <TodoCard key={item._id} item={item} />} />
+      
     </View>
-  );
+  )
 }
 
 const TodoCard = ({ item }: { item: ITodo }) => {
-  const navigation = useNavigation();
-  const { colors } = useTheme();
+  const navigation = useNavigation()
+  const { colors } = useTheme()
   return (
     <CCard>
       <View
@@ -51,28 +44,26 @@ const TodoCard = ({ item }: { item: ITodo }) => {
           flexWrap: 'wrap',
           justifyContent: 'space-between',
           alignItems: 'center',
-        }}>
+        }}
+      >
         <Text
           style={{
             color: colors.text,
             fontWeight: 'bold',
             fontSize: 21,
             width: '60%',
-          }}>
+          }}
+        >
           {item.title}
         </Text>
-        <Switch
-          disabled={true}
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          value={Boolean(item.isCompleted)}
-        />
+        <Switch disabled={true} trackColor={{ false: '#767577', true: '#81b0ff' }} value={Boolean(item.isCompleted)} />
         <CButton
           onPress={() => {
-            navigation.navigate('TodoDetails', { todo: item });
+            navigation.navigate('TodoDetails', { todo: item })
           }}
           title={'Open'}
         />
       </View>
     </CCard>
-  );
-};
+  )
+}
